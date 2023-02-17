9ecea177e276f14f774dec5b1c770a5ee5c38884
Jaja Chang <chihchia.chang@connect.qut.edu.au>
2023-02-18 02:26:43 +1000

    Add home and loggin page

---
+++ flutter_chatapp/lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:279509278263:web:bf14e0e63a61ff667f69b2";
+  static String apiKey = "AIzaSyDY8vpTjPaxZME0XH8FlDUp8H11cNW-bbE";
+  static String messagingSenderId = "279509278263";
+  static String projectId = "flutter-chatapp-9aa14";
+}
