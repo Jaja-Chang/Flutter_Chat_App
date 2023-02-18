5dd8eee8914b41037cdde22504eef39976efd799
Jaja Chang <chihchia.chang@connect.qut.edu.au>
2023-02-18 22:02:21 +1000

    Complete register page UI

--- flutter_chatapp/lib/pages/auth/register_page.dart
+++ flutter_chatapp/lib/pages/auth/register_page.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chatapp/pages/auth/login_page.dart';
+
+import '../../widgets/widgets.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -8,12 +12,162 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final formKey = GlobalKey<FormState>();
+  String fullName = "";
+  String email = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("RegisterPage"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "Friends",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text("Create an account now!",
+                    style:
+                        TextStyle(fontSize: 12, fontWeight: FontWeight.w400)),
+                Image.asset("assets/register.png"),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(
+                      labelText: "Full Name",
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Theme.of(context).primaryColor,
+                      )),
+                  onChanged: (val) {
+                    setState(() {
+                      email = val;
+                      print(email);
+                    });
+                  },
+                  // check the validation (not sure if there's a shorter version)
+                  validator: (val) {
+                    if (val!.isNotEmpty) {
+                      return null;
+                    }
+                    return "Please enter your name!";
+                  },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(
+                      labelText: "Email",
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Theme.of(context).primaryColor,
+                      )),
+                  onChanged: (val) {
+                    setState(() {
+                      fullName = val;
+                    });
+                  },
+                  // check the validation (not sure if there's a shorter version)
+                  validator: (val) {
+                    if (val != null) {
+                      return RegExp(
+                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]")
+                              .hasMatch(val)
+                          ? null
+                          : "Please enter a valid email!";
+                    }
+                    return "Please enter an email!";
+                  },
+                ),
+                const SizedBox(height: 15),
+                TextFormField(
+                  obscureText: true,
+                  decoration: textInputDecoration.copyWith(
+                      labelText: "Password",
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Theme.of(context).primaryColor,
+                      )),
+
+                  // add more constrains for passwords
+                  validator: (val) {
+                    if (val != null) {
+                      if (val.length < 8) {
+                        return "Password must be at least 8 characters";
+                      }
+                      return null;
+                    }
+                    return "Please enter a password!";
+                  },
+                  onChanged: (val) {
+                    setState(() {
+                      password = val;
+                      print(password);
+                    });
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        primary: Theme.of(context).primaryColor),
+                    child: const Text(
+                      "Join!",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 16),
+                    ),
+                    onPressed: () {
+                      register();
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text.rich(TextSpan(
+                  text: "Already have an account? ",
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 0), fontSize: 14),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: "Login!",
+                        style: const TextStyle(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            decoration: TextDecoration.underline),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            nextScreen(context, const LoginPage());
+                          })
+                  ],
+                ))
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
+
+  // dummy register function
+  register() {}
 }
