0019630d518ab9b5b36d15f7226fb07fd9c3e667
Jaja Chang <chihchia.chang@connect.qut.edu.au>
2023-02-18 23:57:59 +1000

    Stay loggedin after logging in successfully

--- flutter_chatapp/lib/main.dart
+++ flutter_chatapp/lib/main.dart
@@ -43,7 +43,7 @@ class _MyAppState extends State<MyApp> {
   getUserLoggedInStatus() async {
     await HelperFunctions.getUserLoggedInStatus().then((value) {
       if (value != null) {
-        _isSignedIn = true;
+        _isSignedIn = value;
       }
     });
   }
