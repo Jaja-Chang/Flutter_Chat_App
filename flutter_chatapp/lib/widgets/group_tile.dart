25dd7a4263e7b5573d872720f337fdf5b163550c
Jaja Chang <chihchia.chang@connect.qut.edu.au>
2023-02-20 15:37:44 +1000

    Link to chat page

--- flutter_chatapp/lib/widgets/group_tile.dart
+++ flutter_chatapp/lib/widgets/group_tile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chatapp/widgets/widgets.dart';
+
+import '../pages/chat_page.dart';
 
 class GroupTile extends StatefulWidget {
   final String userName;
@@ -19,26 +22,37 @@ class GroupTile extends StatefulWidget {
 class _GroupTileState extends State<GroupTile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-      child: ListTile(
-        leading: CircleAvatar(
-          radius: 30,
-          backgroundColor: Theme.of(context).primaryColor,
-          child: Text(
-            widget.groupName.substring(0, 1).toUpperCase(),
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-                color: Colors.white, fontWeight: FontWeight.w500),
+    return GestureDetector(
+      onTap: () {
+        nextScreen(
+            context,
+            ChatPage(
+              groupId: widget.groupId,
+              groupName: widget.groupName,
+              userName: widget.userName,
+            ));
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 30,
+            backgroundColor: Theme.of(context).primaryColor,
+            child: Text(
+              widget.groupName.substring(0, 1).toUpperCase(),
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.w500),
+            ),
           ),
-        ),
-        title: Text(
-          widget.groupName,
-          style: const TextStyle(fontWeight: FontWeight.w300),
-        ),
-        subtitle: Text(
-          "Join the conversation as ${widget.userName}",
-          style: const TextStyle(fontSize: 10),
+          title: Text(
+            widget.groupName,
+            style: const TextStyle(fontWeight: FontWeight.w300),
+          ),
+          subtitle: Text(
+            "Join the conversation as ${widget.userName}",
+            style: const TextStyle(fontSize: 10),
+          ),
         ),
       ),
     );
