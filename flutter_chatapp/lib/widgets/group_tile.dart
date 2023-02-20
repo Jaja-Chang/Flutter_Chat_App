a71519774bbe5b74b6dcdac387c56992a2451541
Jaja Chang <chihchia.chang@connect.qut.edu.au>
2023-02-20 15:17:28 +1000

    Show chats on homepage

--- flutter_chatapp/lib/widgets/group_tile.dart
+++ flutter_chatapp/lib/widgets/group_tile.dart
@@ -19,9 +19,28 @@ class GroupTile extends StatefulWidget {
 class _GroupTileState extends State<GroupTile> {
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(widget.groupId),
-      subtitle: Text(widget.groupName),
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          backgroundColor: Theme.of(context).primaryColor,
+          child: Text(
+            widget.groupName.substring(0, 1).toUpperCase(),
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.w500),
+          ),
+        ),
+        title: Text(
+          widget.groupName,
+          style: const TextStyle(fontWeight: FontWeight.w300),
+        ),
+        subtitle: Text(
+          "Join the conversation as ${widget.userName}",
+          style: const TextStyle(fontSize: 10),
+        ),
+      ),
     );
   }
 }
